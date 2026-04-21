041f72c589e9276d55930dcaa92f95c5ebd84ae8
scartisp <simion.cartis1@gmail.com>
2026-04-20 20:21:24 -0400

    made bar chart vor vis 3

--- visualizationThree/visualizationThree.py
+++ visualizationThree/visualizationThree.py
@@ -5,7 +5,6 @@ import plotly.express as px
 
 def changeFig(fig):
     fig.update_layout(
-        plot_bgcolor='black',
         font_family='balto',
         title_font_size=30
     )
@@ -14,16 +13,28 @@ def changeFig(fig):
 incomeVoteDf = pd.read_csv('../data/incomeVote.csv')
 incomeVoteDf = incomeVoteDf[['Income Amount', 'Percent Reported Voted']]
 
-incomeVoteFig = px.histogram(incomeVoteDf, x='Income Amount', y='Percent Reported Voted', title= 'Percent Reported Voted by Income Range', color_discrete_sequence= px.colors.qualitative.G10)
+incomeVoteFig = px.histogram(incomeVoteDf, x='Income Amount', y='Percent Reported Voted', title= 'Percent Reported Voted by Income Range, 2024', color_discrete_sequence= px.colors.qualitative.G10)
 incomeVoteFig.update_layout(bargap=0.001)
-incomeVoteFig.update_yaxes(title='Percent Reported Voted')
+incomeVoteFig.update_yaxes(title='Percent Voted')
 incomeVoteFig.update_xaxes(title='Income Amount in USD')
-incomeVoteFig.update_layout(
-        font_family='balto',
-        title_font_size=30
-    )
+changeFig(incomeVoteFig)
 incomeVoteFig.update_traces(
-    hovertemplate= 'Income Amount: %{x}<br>' + 'Percent Reported voted: %{y}%<extra></extra>'
+    hovertemplate= 'Income Amount: %{x}<br>' + 'Percent voted: %{y}%<extra></extra>'
 )
 incomeVoteFig.show()
 
+raceVoteDf = pd.read_csv('../data/racialVote.csv')
+
+raceVoteDf = raceVoteDf.drop(raceVoteDf.columns[[0,2,3,4,9,10]], axis=1)
+raceVoteDf = raceVoteDf.head(1)
+#since the income vote data is only for the year 2024, we only want info from 2024
+raceVoteDf = raceVoteDf.melt(id_vars='Year', var_name='Race', value_name='Voting Percentage')
+raceVoteDf = raceVoteDf[['Race', 'Voting Percentage']]
+raceVoteFig = px.bar(raceVoteDf, x='Race', y='Voting Percentage', color='Race', title='Voting Percentage by Race, 2024', color_discrete_sequence= px.colors.qualitative.G10)
+changeFig(raceVoteFig)
+raceVoteFig.update_traces(
+    hovertemplate= 'Race: %{x}<br>' + 'Percent voted: %{y}%<extra></extra>'
+)
+#! I don't really know if I like this bar chart, feels a little needless. Might want to scrap and just replace with layered histogram
+#! maybe I do like it, idk
+raceVoteFig.show()
